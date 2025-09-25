67fd538def980845f6627df1ead5eae7a5aba445
anivnssb <anvinssb@gmail.com>
2025-09-25 22:09:15 +0530

    redux boilerplate code, initial state types

---
+++ src/store/habit-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+export interface Habit {
+  id: string;
+  name: string;
+  frequency: "daily" | "weekly";
+  completedDates: string[];
+  createdAt: string;
+}
+
+interface HabitState {
+  habits: Habit[];
+}
+
+const initialState: HabitState = { habits: [] };
+export const habitSlice = createSlice({
+  name: "habit",
+  initialState,
+  reducers: {},
+});
+
+export const {} = habitSlice.actions;
+
+export default habitSlice.reducer;
