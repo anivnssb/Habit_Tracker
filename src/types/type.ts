7346872127c4352e9ea25aac797d1e28890da57c
anivnssb <anvinssb@gmail.com>
2025-09-26 15:04:16 +0530

    removed all files migrating to next js

--- src/types/type.ts
+++
@@ -1,9 +0,0 @@
-export type HabitFrequency = "daily" | "weekly";
-
-export interface Habit {
-  id: string;
-  name: string;
-  frequency: HabitFrequency;
-  completedDates: string[];
-  createdAt: string;
-}
