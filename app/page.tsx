73f75fca455991b7e81820ae4290b491346503fe
anivnssb <anvinssb@gmail.com>
2025-09-26 20:52:19 +0530

    add habit form component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Button from "@mui/material/Button";
 import styles from "./page.module.css";
+import AddHabitForm from "./_components/AddHabitForm";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return <AddHabitForm />;
 }
