b24cd81653efd42e4baaa970f1e135abd1633f11
anivnssb <anvinssb@gmail.com>
2025-09-30 20:40:58 +0530

    schema changes

--- app/api/createtask/route.ts
+++ app/api/createtask/route.ts
@@ -3,9 +3,11 @@ import { db } from "../../../db/db";
 import { taskTable } from "../../../model/schema";
 
 export async function GET(request: NextRequest) {
-  const newTask: typeof taskTable.$inferInsert = {
-    task_name: "study",
-  };
-  await db.insert(taskTable).values(newTask);
-  console.log("New user created!");
+  console.log(request);
+  // const newTask: typeof taskTable.$inferInsert = {
+  //   task_name: "study",
+  //   frequency: 10,
+  // };
+  // await db.insert(taskTable).values(newTask);
+  // console.log("New user created!");
 }
--- model/schema.ts
+++ model/schema.ts
@@ -1,5 +1,8 @@
-import { int, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, serial, varchar, json } from "drizzle-orm/mysql-core";
+
 export const taskTable = mysqlTable("tasks_table", {
   id: serial().primaryKey(),
   task_name: varchar({ length: 255 }).notNull(),
+  frequency: int().notNull(),
+  completed_dates: json(),
 });
