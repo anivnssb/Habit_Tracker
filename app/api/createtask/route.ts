c34b141b22f86b1ec5dfc53e9083dd13d81143df
anivnssb <anvinssb@gmail.com>
2025-10-02 12:19:30 +0530

    create task api console.log changes

--- app/api/createtask/route.ts
+++ app/api/createtask/route.ts
@@ -3,10 +3,9 @@ import { db } from "../../../db/db";
 import { taskTable } from "../../../model/schema";
 
 export async function POST(request: NextRequest) {
-  console.log("printing request");
   const { task_name, frequency } = await request.json();
   const newTask: typeof taskTable.$inferInsert = { task_name, frequency };
   const task = await db.insert(taskTable).values(newTask);
-  console.log(user);
+  console.log(task);
   return NextResponse.json({ message: "New user created!" }, { status: 200 });
 }
